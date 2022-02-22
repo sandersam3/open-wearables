4d80a0b05698a941dc3e9f88e6eacfbaad6c29e8
sandersam3 <77383660+sandersam3@users.noreply.github.com>
2022-02-22 13:57:05 +0100

    Create test.vue

---
+++ test.vue
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+
+createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+}).mount('#app')
+
+<div id="app">
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</div>
